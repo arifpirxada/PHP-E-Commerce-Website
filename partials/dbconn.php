93d459e70ee68fd221941fb59cbc024cf3a02d5d
Arif <arifpirxada@gmail.com>
2023-10-10 15:49:52 +0530

    Fix - Stored db_host, username, password in .env file

--- partials/dbconn.php
+++ partials/dbconn.php
@@ -1,8 +1,26 @@
 <?php
 session_start();
-$host = "localhost";
-$name = "root";
-$password = "";
+
+// Import env variables ->
+
+$envFilePath = __DIR__ . '/.env';
+
+if (file_exists($envFilePath)) {
+    $lines = file($envFilePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($lines as $line) {
+        list($key, $value) = explode('=', $line, 2);
+        if ($key && $value) {
+            putenv("$key=$value");
+        }
+    }
+} else {
+    die("Env file not found");
+}
+
+$host = getenv('DB_HOST');
+$name = getenv('USER');
+$password = getenv("DB_PASS");
 $database = "e-commerce";
 
 $conn = mysqli_connect($host,$name,$password,$database);
